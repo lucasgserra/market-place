078fd6111e6b8190c8ceb5c75b5fc2ec3719cdb2
lucas serra <lucasserradevelopment@gmail.com>
2025-03-24 20:10:57 -0300

    Initial commit

---
+++ src/main/kotlin/dev/lucasserra/market_place/MarketPlaceApplication.kt
@@ -0,0 +1,11 @@
+package dev.lucasserra.market_place
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketPlaceApplication
+
+fun main(args: Array<String>) {
+	runApplication<MarketPlaceApplication>(*args)
+}
